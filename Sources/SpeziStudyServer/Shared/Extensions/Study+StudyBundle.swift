da1c42feca03a7ada7beb4ade481bb5c3513ff21
Philipp Nagy <philipp.nagy@tum.de>
2026-01-24 15:33:42 +0100

    Add copyright and licensing information

--- Sources/SpeziStudyServer/Shared/Extensions/Study+StudyBundle.swift
+++ Sources/SpeziStudyServer/Shared/Extensions/Study+StudyBundle.swift
@@ -1,8 +1,9 @@
 //
-//  Study+StudyBundle.swift
-//  SpeziStudyServer
+// This source file is part of the SpeziStudyServer open source project
 //
-//  Created by Philipp Nagy on 20.01.26.
+// SPDX-FileCopyrightText: 2026 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
 //
 
 import Foundation
